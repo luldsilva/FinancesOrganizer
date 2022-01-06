bdacb858ffade94228cb35d8a00461d79dcd1b00
Lucas Miha <lucaslisilva@outlook.com>
2022-01-05 22:01:07 -0300

    Add DTO model, conn string and first route for test

---
+++ FinancesOrganizer/Controllers/FinancesController.cs
@@ -0,0 +1,22 @@
+﻿using FinancesOrganizer.Models;
+using FinancesOrganizer.Models.DTOS;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinancesOrganizer.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+
+    public class FinController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetTest()
+        {
+            return Ok("API respondendo!");
+        }
+    }
+}
---
+++ FinancesOrganizer/Models/DTOS/CreateDataWallet.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinancesOrganizer.Models.DTOS
+{
+    public class CreateDataWallet
+    {
+        [Required(ErrorMessage ="O campo nome do títular é obrigatório")]
+        public string Nome_titular { get; private set; }
+        
+        [Required(ErrorMessage ="O campo saldo é obrigatório")]
+        public double Saldo { get; private set; }
+    }
+}

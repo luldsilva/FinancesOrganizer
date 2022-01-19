939376678671a825b48cc2e30f86d291846f1459
Lucas Miha <lucaslisilva@outlook.com>
2022-01-18 22:34:34 -0300

    Created CRUD basic asnd add auto mapper

--- FinancesOrganizer/Controllers/FinancesController.cs
+++ FinancesOrganizer/Controllers/FinancesController.cs
@@ -1,4 +1,5 @@
-﻿using FinancesOrganizer.Data;
+﻿using AutoMapper;
+using FinancesOrganizer.Data;
 using FinancesOrganizer.Models;
 using FinancesOrganizer.Models.DTOS;
 using FinancesOrganizer.Repositories;
@@ -17,20 +18,69 @@ namespace FinancesOrganizer.Controllers
     {
         private RecipeRepository _repo;
         private DataContext _context;
+        private IMapper _mapper;
 
-        public FinancesController(DataContext context)
+        public FinancesController(DataContext context, IMapper mapper)
         {
             //_repo = repo;
             _context = context;
+            _mapper = mapper;
         }
 
         [HttpPost]
-        public IActionResult Recipe([FromBody] Recipe recipe)
+        public IActionResult Recipe([FromBody] CreateRecipeDTO recipeDTO)
         {
+            Recipe recipe = _mapper.Map<Recipe>(recipeDTO);
             //_repo.RegisterRecipe(recipe);
             _context.Recipe.Add(recipe);
             _context.SaveChanges();
             return Ok();
+        }
+
+        [HttpGet]
+        public IActionResult ListRecipe()
+        {
+            return Ok(_context.Recipe);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult RecoveryRecipe(long id)
+        {
+            Recipe recipe = _context.Recipe.FirstOrDefault(recipe => recipe.Id == id);
+            if(recipe == null)
+            {
+                return NotFound();
+            }
+            ReadRecipeDTO recipeDTO = _mapper.Map<ReadRecipeDTO>(recipe);
+            return Ok(recipeDTO);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateRecipe(long id, [FromBody] UpdateRecipeDTO newRecipeDTO)
+        {
+            Recipe recipe = _context.Recipe.FirstOrDefault(recipe => recipe.Id == id);
+            if(recipe == null)
+            {
+                return NotFound();
+            }
+
+            _mapper.Map(newRecipeDTO, recipe);
+            _context.SaveChanges();
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteRecipe(long id)
+        {
+            Recipe recipe = _context.Recipe.FirstOrDefault(recipe => recipe.Id == id);
+            if (recipe == null)
+            {
+                return NotFound();
+            }
+
+            _context.Remove(recipe);
+            _context.SaveChanges();
+            return NoContent();
         }
     }
 }

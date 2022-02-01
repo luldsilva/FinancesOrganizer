b3979d99facfa575da020456a8eb0994c0feef62
Lucas Miha <lucaslisilva@outlook.com>
2022-01-31 23:41:46 -0300

    Add system for register users

---
+++ UsuariosAPI/Data/Dtos/RegisterUserDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UsuariosAPI.Data.Dtos
+{
+    public class RegisterUserDTO
+    {
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required]
+        [Compare("Password")]
+        public string RePassword { get; set; }
+    }
+}
---
+++ UsuariosAPI/Profiles/UserProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UsuariosAPI.Data.Dtos;
+using UsuariosAPI.Models;
+
+namespace UsuariosAPI.Profiles
+{
+    public class UserProfile : Profile
+    {
+        public UserProfile()
+        {
+            CreateMap<RegisterUserDTO, User>();
+            CreateMap<User, IdentityUser<int>>();
+        }
+    }
+}

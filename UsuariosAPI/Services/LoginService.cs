058c7cbe971563c204743cd99bf6b77b5876f9da
Lucas Miha <lucaslisilva@outlook.com>
2022-02-01 21:49:06 -0300

    Add system login , logout and token validation

---
+++ UsuariosAPI/Services/LoginService.cs
@@ -0,0 +1,39 @@
+﻿using FluentResults;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UsersAPI.Data.Requests;
+using UsersAPI.Models;
+
+namespace UsersAPI.Services
+{
+    public class LoginService
+    {
+        private SignInManager<IdentityUser<int>> _signInManager;
+        private TokenService _tokenService;
+
+        public LoginService(SignInManager<IdentityUser<int>> signInManager, TokenService tokenService)
+        {
+            _signInManager = signInManager;
+            _tokenService = tokenService;
+        }
+
+        public Result LogaUsuario(LoginRequest request)
+        {
+            var resultIdentity = _signInManager.PasswordSignInAsync(request.UserName, request.Password, false, false);
+            if (resultIdentity.Result.Succeeded) 
+            {
+                var identityUser = _signInManager
+                    .UserManager
+                    .Users
+                    .FirstOrDefault(user =>
+                    user.NormalizedUserName == request.UserName.ToUpper());
+                Token token = _tokenService.CreateToken(identityUser);
+                return Result.Ok().WithSuccess(token.Value);
+            }
+            return Result.Fail("Login falhou");
+        }
+    }
+}
---
+++ UsuariosAPI/Services/TokenService.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using UsersAPI.Models;
+
+namespace UsersAPI.Services
+{
+  public class TokenService
+  {
+    public Token CreateToken(IdentityUser<int> usuario)
+    {
+      Claim[] directivesUser = new Claim[]
+      {
+                new Claim("username", usuario.UserName),
+                new Claim("id", usuario.Id.ToString())
+      };
+
+      var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("0asdjas09djsa09djasdjsadajsd09asjd09sajcnzxn"));
+      var credencials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+      var token = new JwtSecurityToken(
+          claims: directivesUser,
+          signingCredentials: credencials,
+          expires: DateTime.UtcNow.AddHours(1)
+          ); ;
+
+      var tokenString = new JwtSecurityTokenHandler().WriteToken(token);
+      return new Token(tokenString);
+    }
+  }
+}

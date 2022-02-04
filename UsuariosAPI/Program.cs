a852b00aa9c0a4ef0bab42bd7c1e18ef79c8e89a
Lucas Miha <lucaslisilva@outlook.com>
2022-02-03 21:25:05 -0300

    add conn ISS

--- UsuariosAPI/Program.cs
+++ UsuariosAPI/Program.cs
@@ -20,7 +20,8 @@ namespace UsuariosAPI
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseStartup<Startup>()
+                    .UseIISIntegration();
                 });
     }
 }
